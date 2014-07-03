bc4c3601bb55bd1a10a1cf3406dcad1242ab43a8
AJ ONeal <awesome@coolaj86.com>
2014-07-03 16:24:27 -0600

    uploading demo to github, yay!

--- README.md
+++ README.md
@@ -1,4 +1,21 @@
-firefox-os-sms-demo
+Firefox OS SMS Demo
 ===================
 
-A demo app for sending and receiving SMS tested on Firefox OS 1.3.0
+A demo app for sending and receiving SMS.
+
+This is the "Hello World" of SMS apps for Firefox OS.
+
+Notes
+=====
+
+  * Tested working in Firefox OS 1.3.0 on the ZTE Open C with a T-Mobile SIM.
+  * Only `manifest.webapp.type == certified` apps get access to SMS APIs.
+  * The API is buggy / incomplete and Mozilla's documentation doesn't always match their implementation
+  * It seems that you can only receive SMS notifications after interacting with the messaging API
+  * I don't know if you can capture the SMS event to `stopPropagation()` up to OS level.
+
+Possible Errors
+======
+
+  * NoSim-something-or-other - the sim card is missing
+  * The `Debug` feature of `App Manager` doesn't seem to work with this... perhaps because it's a `certified` app???
--- js/app.js
+++ js/app.js
@@ -1,3 +1,142 @@
+/*global MozSmsFilter*/
 $(function() {
-  $("div").text("Hello World!");
+  'use strict';
+
+  /*
+  function logObject(obj) {
+    $("#response").append("<br> New Object: " + obj);
+    Object.keys(obj).forEach(function (key) {
+      $("#response").append("<br> " + key + ": " + obj[key]);
+    });
+  }
+  */
+  function logMsg(msg) {
+    [
+      'type', 'id', 'threadId', 'body', 'delivery', 'deliveryStatus', 'read'
+    , 'receiver', 'sender', 'timestamp', 'messageClass'
+    ].forEach(function (key) {
+      $("#response").append("<br> " + key + ": " + msg[key]);
+    });
+  }
+
+  $('form#sms-form').on("submit", function (ev) {
+    $("#response").html("submit form");
+    ev.preventDefault();
+    ev.stopPropagation();
+
+    var msg = $('[name=msg]').val()
+      , phone = $('[name=phone]').val()
+      , request
+      //, requests
+      ;
+
+    $("#response").html("got values");
+
+    request = navigator.mozMobileMessage.send(phone, msg);
+    $("#response").html("tried to send message");
+
+    //requests.forEach(function (request) {
+    $("#response").html("iterate through requests");
+    request.onsuccess = function () {
+      window.thing = this;
+      console.error(this.result);
+      $("#response").html("Sent to <br>" + this.result);
+      logMsg(this.result);
+    };
+    request.onerror = function () {
+      window.thing = this;
+      console.error(this.error.name);
+      console.error(this.error.message);
+      $("#response").html(this.error.name + ':' + this.error.message);
+    };
+    //});
+  });
+
+  function showMessages(id) {
+    var filter = new MozSmsFilter() // https://developer.mozilla.org/en-US/docs/Web/API/MozSmsFilter
+      , cursor
+      ;
+
+    filter.read = false;
+    if ('undefined' !== typeof id) {
+      filter.threadId = id;
+    }
+
+    // Get the messages from the latest to the first
+    cursor = navigator.mozMobileMessage.getMessages(filter, true);
+
+    cursor.onsuccess = function () {
+      logMsg(this.result);
+      /*
+      var message = this.result
+        , time = message.timestamp.toDateString()
+        ;
+
+      console.log(time + ': ' + (message.body || message.subject)); // SMS || MMS
+      $("#response").append("<div>Got new message [" + time + "]"
+        + "<br>" + (message.body || message.subject)
+        + "</div>"
+      );
+
+      if (!this.done) {
+        this.continue();
+      }
+      */
+    };
+  }
+
+  // 'received' seems to only activate after a message is sent
+  navigator.mozMobileMessage.addEventListener('received', function (msg) {
+    // https://developer.mozilla.org/en-US/docs/Web/API/MozSmsMessage
+    $("#response").html("Got a message");
+    $("#response").append("<br>" + msg);
+    logMsg(msg);
+
+    showMessages(msg.id);
+  });
+
+  navigator.mozSetMessageHandler("alarm", function (mozAlarm) { 
+    // var directive = alorms[mozAlarm.data.id]; doStuff(directive);
+    $("#alarms").append("<li>Alarm Fired: " + new Date().toString().replace(/GMT.*/, ''));
+      //+ " : " + JSON.stringify(mozAlarm.data) + "</li>");
+    showMessages();
+    //setAlarm(7 * 1000);
+    setAlarm(7 * 60 * 1000);
+  });
+  
+  function setAlarm(offset) {
+    var alarmId
+      , date
+      , request
+      ;
+
+    $("#response").append("<br><br> setting alarm for " + (offset / 1000) + "s in the future");
+    date = new Date(Date.now() + offset);
+    $("#response").append("<br> will fire at " + " for " + date.toString().replace(/GMT.*/, ''));
+
+    // Set an alarm and store it's id
+    //request = navigator.mozAlarms.add(date, "ignoreTimezone", { foo: "bar" });
+    request = navigator.mozAlarms.add(date, "honorTimezone", { foo: "bar" });
+
+    request.onsuccess = function () {
+      // alarms[this.result] = { type: 'thingy', params: ['do', 'stuff'] };
+      alarmId = this.result;
+      if (this.result) {
+        $("#response").append("<br> set alarm " + this.result + " for " + date.toString().replace(/GMT.*/, ''));
+      } else {
+        $("#response").append("<br><br> error setting alarm: " + this.error);
+      }
+    };
+
+    // ...
+
+    // Later on, removing the alarm if it exists
+    if (alarmId) {
+      navigator.mozAlarms.remove(alarmId);
+    }
+  }
+
+  if (!navigator.mozHasPendingMessage("alarm")) {
+    setAlarm(5 * 1000);
+  }
 });
